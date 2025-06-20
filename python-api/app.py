29fdb63de9ca7adf1e2c53a07429716fd320edfb
dmplastun <kartmen9@gmail.com>
2025-06-20 16:10:33 +0300

    Initial commit

---
+++ python-api/app.py
@@ -0,0 +1,12 @@
+# app.py
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return f"Hello from Python Microservice 2! Env: {os.getenv('APP_ENV')}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
